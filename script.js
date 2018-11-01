8288ad072bfac1238ed110d75cc1c7ad84e44b92
deadlylaid <deadlylaid@gmail.com>
2018-11-01 23:16:24 +0900

    querySelector -> popup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //컨텐츠 페이지를 대상으로 코드를 실행해주세요. 
 chrome.tabs.executeScript({
-  code:'var bodyText = document.querySelector("body").innerText;alert(bodyText);'
+  code:'var bodyText = document.getSelection().toString();alert(bodyText);'
 });
